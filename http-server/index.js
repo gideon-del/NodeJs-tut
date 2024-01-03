cdc2937aad27fc89651884338e2016e3ac8f9dcd
gideon-del <gideonchidi471@gmail.com>
2024-01-03 13:50:39 +0100

    chore: post and get request

--- http-server/index.js
+++ http-server/index.js
@@ -1,20 +1,41 @@
 const http = require("http");
-const PORT = 3000;
+const PORT = 5000;
 const server = http.createServer();
+const friends = [
+  {
+    id: 0,
+    name: "Nikola Tesla",
+  },
+  {
+    id: 1,
+    name: "Sir Isaax",
+  },
+];
 server.on("request", (req, res) => {
-  if (req.url === "/friends") {
-    // res.writeHead(200, {
-    //   "Content-Type": "application/json",
-    // });
+  const items = req.url.split("/");
+  // /firends/2 => ['', 'friends', '2]
+  if (req.method === "POST" && items[1] === "friends") {
+    req.on("data", (data) => {
+      const friend = data.toString();
+      console.log(`Request: ${friend}`);
+      friends.push(JSON.parse(friend));
+    });
+  } else if (req.method == "GET" && items[1] === "friends") {
     res.statusCode = 200;
     res.setHeader("Content-Type", "application/json");
-    res.end(
-      JSON.stringify({
-        name: "Sir Isacc Newton",
-        id: 1,
-      })
-    );
-  } else if (req.url === "/messages") {
+    if (items.length === 3) {
+      const friendIndex = friends.findIndex(
+        (friend) => friend.id === Number(items[2])
+      );
+      if (friendIndex < 0) {
+        res.statusCode = 404;
+        res.end();
+      }
+      res.end(JSON.stringify(friends[friendIndex]));
+    } else {
+      res.end(JSON.stringify(friends));
+    }
+  } else if (req.method === "GET" && items[1] === "/messages") {
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
     res.write("<body>");
