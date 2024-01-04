bed521fd8fd9be6bdc51261aad7e048fe2bcbd0d
gideon-del <gideonchidi471@gmail.com>
2024-01-04 14:11:14 +0100

    chore: added data from csv file

--- nasa-project/server/src/server.js
+++ nasa-project/server/src/server.js
@@ -1,8 +1,13 @@
 const http = require("http");
 const app = require("./app");
+const { loadPlanets } = require("./models/planets.model");
 
 const server = http.createServer(app);
 const PORT = process.env.PORT || 8000;
-server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+async function startServer() {
+  await loadPlanets();
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
+}
+startServer();
