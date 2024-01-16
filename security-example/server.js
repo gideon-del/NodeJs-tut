c7c29cc6cdfa3ed1a04099c9272a83526e364e09
gideon-del <gideonchidi471@gmail.com>
2024-01-16 11:43:00 +0100

    chore: added cookie session

--- security-example/server.js
+++ security-example/server.js
@@ -1,12 +1,39 @@
+require("dotenv").config();
 const fs = require("fs");
 const path = require("path");
 const https = require("https");
 const express = require("express");
 const helmet = require("helmet");
+const passport = require("passport");
+const { Strategy } = require("passport-google-oauth20");
+const cookieSession = require("cookie-session");
+const { verify } = require("crypto");
 const PORT = 3000;
-
+const config = {
+  CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  CLIENT_SECRET: process.env.GOOGLE_SECRET,
+};
+const AUTH_OPTIONS = {
+  callbackURL: "/auth/google/callback",
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET,
+};
+function verifyCallback(accessToken, refreshToken, profile, done) {
+  console.log("Google profile", profile);
+  done(null, profile);
+}
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
 const app = express();
+
 app.use(helmet());
+app.use(
+  cookieSession({
+    name: "session",
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: ["secret key"],
+  })
+);
+app.use(passport.initialize());
 function chekedLoggedIn(req, res, next) {
   const isLoggedIn = true; //TODO
   if (!isLoggedIn) {
@@ -14,12 +41,30 @@ function chekedLoggedIn(req, res, next) {
   }
   next();
 }
-app.get("/auth/google", (req, res) => {});
-app.get("/auth/google/callback", (req, res) => {});
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["email"],
+  })
+);
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/failure",
+    successRedirect: "/",
+    session: false,
+  }),
+  (req, res) => {
+    console.log("Google called us back");
+  }
+);
 app.get("/auth/logout", (req, res) => {});
 
 app.get("/secret", chekedLoggedIn, (req, res) => {
   return res.send("Your personal secret value is 41");
+});
+app.get("/failure", (req, res) => {
+  return res.send("Failed to login");
 });
 app.get("/", (req, res) => {
   return res.sendFile(path.join(__dirname, "public", "index.html"));
